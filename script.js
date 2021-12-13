89594b9ef89f0c5ab6d419152f1517ca2715665c
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-13 17:32:19 -0500

    updated JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+function getAnswer() {
+    document.getElementById("answer").innerHTML = "Hello again Dear Visitor!</br> Welcome to my world! I'm Cécilia and this is who I am: <ol><li>Professional Dancer</li><li>Yoga and Meditation Teacher</li><li>Wellness Coach</li><li>Soon to be Software Engineer</li></ol>Did you guess correctly?";
+};
+
+
+
 
